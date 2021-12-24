27dcbfc07209797d98b343bd1186e890b3e6e7a3
Andrew Cole <rampantleaf@gmail.com>
2021-12-24 01:58:15 -0400

    Added debug character code shortcut, tweaked about.html

--- js/patches.js
+++ js/patches.js
@@ -1,23 +1,36 @@
 // New flags
-var DEBUG = false;
+var DEBUG = true;
 
 var halloweenEnabled = true;
 var holidaysEnabled = true;
 var valentinesEnabled = true;
 var easterEnabled = true;
 
-// Debug shortcut (c key)
+// View character key debug shortcut (c key)
 document.addEventListener("keydown", (e) => {
-    if (e.keyCode == 67 && DEBUG) {
-        peanuts.speciesselect.createDog();
-        peanuts.dogselect.createMediumDog();
-        window.setTimeout(() => {peanuts.creator.partsMenu.select(1);}, 2000);
+    if (e.keyCode == 67 && DEBUG && (peanuts.section.currentSection === "creator" || peanuts.section.currentSection === "ending")) {
+        document.getElementsByClassName("deny")[0].style.display = "none";
+        document.querySelector(".alert .message").style.userSelect = "text";
+        peanuts.alert.message({
+            message: "Your character code: " + peanuts.creator.data.key,
+            confirm: "Close",
+            deny: "",
+            callback: viewKeyCallback
+        })
     }
 });
 
+// View character key callback
+function viewKeyCallback(e) {
+    window.setTimeout(() => {
+        document.getElementsByClassName("deny")[0].style = "";
+        document.querySelector(".alert .message").style.userSelect = "none";
+    }, 500)
+}
+
 // CTRL+S save shortcut
 document.addEventListener("keydown", function(e) {
-      if ((window.navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)  && e.keyCode == 83) {
+      if ((window.navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)  && e.keyCode == 83 && DEBUG) {
         e.preventDefault();
 
         if (peanuts.section.currentSection === "creator") {
